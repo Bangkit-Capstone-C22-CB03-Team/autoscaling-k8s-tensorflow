040ccc2005736903cbdf244b00be0586ef6f7d73
Aclaputra <muhamadacla22@gmail.com>
2022-05-29 21:31:10 +0700

    feat: added default autoscaling k8s tensoflor serving

---
+++ kubernetes/load-test-model.sh
@@ -0,0 +1,19 @@
+EXTERNAL_IP=34.133.47.169
+echo "Verify that the model is up and operational."
+curl -d @locust/request-body.json -X POST http://$EXTERNAL_IP:8501/v1/models/image_classifier:predict
+
+echo "Configure Horizontal Pod Autoscaler."
+kubectl autoscale deployment image-classifier --cpu-percent=60 --min=1 --max=4
+
+echo "Check the status of the autoscaler."
+kubectl get hpa
+
+echo "-Your k8s Cluster, deployment, service have been added with Horizontal Pod Autoscaling-"
+echo "-Next try run load-test-model.sh for-"
+
+echo "Load test the model"
+cd locust
+locust -f tasks.py --headless --users 32 --spawn-rate 1 --step-load --step-users 1 --step-time 30s --host http://$EXTERNAL_IP:8501
+
+echo "-Observe the TF Serving Deployment in GKE dashboard.-"
+echo "-Observe the default node-pool-"
