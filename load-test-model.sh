80d6f2b06c3ea8762831b77615f52034f5d5be0e
Aclaputra <muhamadacla22@gmail.com>
2022-05-29 22:50:29 +0700

    fix: modified locust load test model

--- load-test-model.sh
+++ load-test-model.sh
@@ -1,4 +1,7 @@
-EXTERNAL_IP=34.133.47.169
+kubectl get svc
+EXTERNAL_IP=34.66.250.249
+echo "-Install specific locust version-"
+pip3 install locust==1.4.1
 echo "Verify that the model is up and operational."
 curl -d @locust/request-body.json -X POST http://$EXTERNAL_IP:8501/v1/models/image_classifier:predict
 
@@ -13,7 +16,12 @@ echo "-Next try run load-test-model.sh for-"
 
 echo "Load test the model"
 cd locust
-locust -f tasks.py --headless --users 32 --spawn-rate 1 --step-load --step-users 1 --step-time 30s --host http://$EXTERNAL_IP:8501
+locust -f tasks.py \
+--headless \
+--host http://${EXTERNAL_IP}:8501
+
+# cd locust
+# locust -f tasks.py --headless --users 32 --spawn-rate 1 --step-load --step-users 1 --step-time 30s --host http://$EXTERNAL_IP:8501
 
 echo "-Observe the TF Serving Deployment in GKE dashboard.-"
 echo "-Observe the default node-pool-"
